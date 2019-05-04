5cf223048bfc7b3bb35b70ab9ff8d423a041e505
loringmuller <loringmuller@gmail.com>
2019-05-04 13:17:24 -0700

    landing page

--- fit-monkeys/src/App.js
+++ fit-monkeys/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "./pages/Signup/";
 import NoMatch from "./pages/NoMatch";
+import Landing from "./pages/Landing";
 import './App.css';
+
+console.log(Landing);
 console.log(Signup);
 const App = () => {
   return (
     <Router>
     <div>
       <Switch>
+        <Route exact path="/" component={Landing} />
         <Route exact path="/signup" component={Signup} />
         <Route component={NoMatch} />
       </Switch>
