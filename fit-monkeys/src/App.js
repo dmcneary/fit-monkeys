db83cf0c5cee6a9e997de7201c53bcd2e0d0ed7e
ayyyyy <mcneary.david@gmail.com>
2019-05-04 17:36:43 -0700

    more views

--- fit-monkeys/src/App.js
+++ fit-monkeys/src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./pages/Landing/";
+import Dashboard from "./pages/Dashboard/";
+import AllActivities from "./pages/AllActivities/";
+import ActivityDetail from "./pages/ActivityDetail/";
+import NewActivity from "./pages/NewActivity/";
+import Challenges from "./pages/Challenges/";
+import Login from "./pages/Login/";
 import Signup from "./pages/Signup/";
 import NoMatch from "./pages/NoMatch";
 import './App.css';
-console.log(Signup);
+
 const App = () => {
   return (
     <Router>
     <div>
       <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/all-activities" component={AllActivities} />
+        <Route exact path="/activities/:id" component={ActivityDetail} />
+        <Route exact path="/newactivity" component={NewActivity} />
+        <Route exact path="/challenges" component={Challenges} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route component={NoMatch} />
       </Switch>
---
+++ fit-monkeys/src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+//import Header from "../../components/Header";
+import { Input } from "../../components/Form";
+import { Col, Row, Container } from "../../components/Grid";
+//import API from "../../utils/API";
+import { Link } from "react-router-dom";
+//import Footer from "../components/Footer";
+
+const Login = (props) => {
+    
+}
