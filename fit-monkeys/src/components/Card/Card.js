a5a058bd5c414482742eec8be06132fc75770b67
loringmuller <loringmuller@gmail.com>
2019-05-07 18:25:03 -0700

    card file

---
+++ fit-monkeys/src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CardBody(props) {
+  return (
+    <div className="card-body">
+      <p className="card-text">Challenges</p>
+    </div>
+  );
+}
+
+export default CardBody;
