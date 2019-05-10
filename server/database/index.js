e1b0c4bbc4331731a8707ede79de1698e2299d18
vinny <shirishvinay@gmail.com>
2019-05-09 18:41:37 -0700

    third commit

---
+++ server/database/index.js
@@ -0,0 +1,21 @@
+//Connect to Mongo database
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const uri = 'mongodb://localhost:27017/simple-mern-passport' 
+
+mongoose.connect(uri).then(
+    () => { 
+        console.log('Connected to Mongo');
+        
+    },
+    err => {
+
+         console.log('error connecting to Mongo: ')
+         console.log(err);
+         
+        }
+  );
+
+
+module.exports = mongoose.connection
