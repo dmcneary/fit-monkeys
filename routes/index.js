e5a1cedac1925613b6697c41d04c118e1f0ccae3
dmcneary <mcneary.david@gmail.com>
2019-05-23 10:37:06 -0700

    heroku debug

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,18 @@ const userRoutes = require('./user')
 router.use("/user", userRoutes);
 router.use("/api", apiRoutes);
 
+
+//serve production build with static routes if necesssary
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+	  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+  }
+  
+//otherwise serve dev build
 router.use(function (req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
