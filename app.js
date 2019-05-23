c342eb1c119f2a1ee102c6b26c42eb37b76baaca
dmcneary <mcneary.david@gmail.com>
2019-05-23 11:17:14 -0700

    heroku debug

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	}
+	app.get('/*', function(req, res) {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	});
 	
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
